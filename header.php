19519f3f8f296ce642cb36bf5aed26b201fbbe5f
takuya <awa08082023@gmail.com>
2023-12-10 05:35:04 +0900

    header.php内のaタグhref中身記述

--- header.php
+++ header.php
@@ -11,6 +11,15 @@
     <?php wp_head(); ?>
 </head>
 
+<?php
+$news = esc_url( home_url('/news/') );
+$service = esc_url( home_url('/service/') );
+$works = esc_url( home_url('/works/') );
+$company = esc_url( home_url('/company/') );
+$recruit = esc_url( home_url('/recruit/') );
+$contact = esc_url( home_url('/contact/') );
+?>
+
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <header class="l-header p-header">
@@ -26,44 +35,44 @@
             <nav class="p-header__pc-nav pc-nav">
                 <ul class="pc-nav__items">
                     <li class="pc-nav__item">
-                        <a href="#">NEWS</a>
+                        <a href="<?php echo $news; ?>">NEWS</a>
                     </li>
                     <li class="pc-nav__item">
-                        <a href="#">SERVICE</a>
+                        <a href="<?php echo $service; ?>">SERVICE</a>
                     </li>
                     <li class="pc-nav__item">
-                        <a href="#">WORKS</a>
+                        <a href="<?php echo $works; ?>">WORKS</a>
                     </li>
                     <li class="pc-nav__item">
-                        <a href="#">COMPANY</a>
+                        <a href="<?php echo $company; ?>">COMPANY</a>
                     </li>
                     <li class="pc-nav__item">
-                        <a href="#">RECRUIT</a>
+                        <a href="<?php echo $recruit; ?>">RECRUIT</a>
                     </li>
                     <li class="pc-nav__item">
-                        <a href="#">CONTACT</a>
+                        <a href="<?php echo $contact; ?>">CONTACT</a>
                     </li>
                 </ul>
             </nav>
             <nav class="p-header__sp-nav sp-nav js-sp-nav">
                 <ul class="sp-nav__items">
                     <li class="sp-nav__item">
-                        <a href="">NEWS</a>
+                        <a href="<?php echo $news; ?>">NEWS</a>
                     </li>
                     <li class="sp-nav__item">
-                        <a href="">SERVICE</a>
+                        <a href="<?php echo $service; ?>">SERVICE</a>
                     </li>
                     <li class="sp-nav__item">
-                        <a href="">WORKS</a>
+                        <a href="<?php echo $works; ?>">WORKS</a>
                     </li>
                     <li class="sp-nav__item">
-                        <a href="">COMPANY</a>
+                        <a href="<?php echo $company; ?>">COMPANY</a>
                     </li>
                     <li class="sp-nav__item">
-                        <a href="">RECRUIT</a>
+                        <a href="<?php echo $recruit; ?>">RECRUIT</a>
                     </li>
                     <li class="sp-nav__item">
-                        <a href="">CONTACT</a>
+                        <a href="<?php echo $contact; ?>">CONTACT</a>
                     </li>
                 </ul>
 
