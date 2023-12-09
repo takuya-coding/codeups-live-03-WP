021fc20179f5a097937b977c9c5f2e53551e9d2a
takuya <awa08082023@gmail.com>
2023-12-09 12:05:26 +0900

    function.php wp_enqueue_style,wp_enqueue_scriptの階層修正

--- functions.php
+++ functions.php
@@ -35,9 +35,9 @@ add_action( 'after_setup_theme', 'my_setup' );
 function my_script_init()
 {
 
-	wp_enqueue_style( 'my', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.1', 'all' );
+	wp_enqueue_style( 'style-css', get_template_directory_uri() . '/css/styles.css', array(), '1.0.1', 'all' );
 
-	wp_enqueue_script( 'my', get_template_directory_uri() . '/assets/js/script.js', array( 'jquery' ), '1.0.1', true );
+	wp_enqueue_script( 'script-js', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0.1', true );
 
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
