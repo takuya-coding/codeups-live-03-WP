2b649c13dca2e1d42bae3a9a177035cc001505d0
takuya <awa08082023@gmail.com>
2023-12-10 17:23:28 +0900

    home.phpにメインループ処理記述

--- home.php
+++ home.php
@@ -1,3 +1,50 @@
 <?php get_header(); ?>
 
+<!-- 下層ページのメインビュー -->
+<div class="p-sub-mv js-mv-height">
+    <div class="p-sub-mv__title">
+        <div class="c-section-header--white">
+            <h1 class="c-section-header__engtitle">news</h1>
+            <p class="c-section-header__jatitle">お知らせ</p>
+        </div>
+    </div>
+</div>
+
+<!-- パンくず -->
+<div class="c-breadcrumb">
+    <div class="l-inner">
+        <div>HOME > NEWS</div>
+    </div>
+</div>
+
+<div class="l-archive-home p-archive-home">
+    <div class="p-archive-home__inner l-inner">
+        <!-- ループ処理開始 -->
+        <?php if (have_posts()): ?>
+        <?php while (have_posts()) : the_post(); ?>
+        <div class="p-archive-home__item p-news-content">
+            <div class="p-news-content__meta">
+                <time datetime="<?php the_time('c') ?>" class="p-news-content__date"><?php the_time('Y.m.d') ?></time>
+                <p class="p-news-content__category">
+                    <?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->cat_name; } ?>
+                </p>
+            </div>
+            <div class="p-news-content__link">
+                <a href="<?php the_permalink(); ?>" class="p-news-content__text"><?php the_title(); ?></a>
+            </div>
+        </div>
+        <?php endwhile; ?>
+        <?php endif; ?>
+        <!-- ループ終了 -->
+    </div>
+</div>
+
+<div class="l-paginavi">
+    <div class="wp-pagenavi">
+        <span class="current">1</span>
+        <a class="page smaller" href="#">2</a>
+        <a class="page smaller" href="#">3</a>
+    </div>
+</div>
+
 <?php get_footer(); ?>
