6f1782659d80e79aa08aabf60389d61cc5af3aff
takuya <awa08082023@gmail.com>
2023-12-20 07:38:05 +0900

    archive-works.php完成

--- archive-works.php
+++ archive-works.php
@@ -18,46 +18,25 @@
     <div class="p-archive-works__inner l-inner">
         <div class="p-archive-works__items">
             <!-- ループ -->
+            <?php if (have_posts()): ?>
+            <?php while (have_posts()) : the_post(); ?>
             <div class="p-archive-works__item">
                 <div class="p-archive-works__img">
-                    <img src="./images/common/works_1.jpg" alt="">
+                    <?php if (has_post_thumbnail()): ?>
+                    <!-- 投稿にアイキャッチ画像が有る場合の処理 -->
+                    <?php the_post_thumbnail(); ?>
+                    <!-- アイキャッチ画像がない場合の処理 -->
+                    <?php else: ?>
+                    <img src="<?php echo get_template_directory_uri() ?>/images/common/noimage.png" alt="">
+                    <?php endif; ?>
                 </div>
                 <div class="p-archive-works__title">
-                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
+                    <h2 class="p-archive-works__name"><?php the_title(); ?> 様</h2>
                 </div>
             </div>
-            <div class="p-archive-works__item">
-                <div class="p-archive-works__img">
-                    <img src="./images/common/works_1.jpg" alt="">
-                </div>
-                <div class="p-archive-works__title">
-                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
-                </div>
-            </div>
-            <div class="p-archive-works__item">
-                <div class="p-archive-works__img">
-                    <img src="./images/common/works_1.jpg" alt="">
-                </div>
-                <div class="p-archive-works__title">
-                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
-                </div>
-            </div>
-            <div class="p-archive-works__item">
-                <div class="p-archive-works__img">
-                    <img src="./images/common/works_1.jpg" alt="">
-                </div>
-                <div class="p-archive-works__title">
-                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
-                </div>
-            </div>
-            <div class="p-archive-works__item">
-                <div class="p-archive-works__img">
-                    <img src="./images/common/works_1.jpg" alt="">
-                </div>
-                <div class="p-archive-works__title">
-                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
-                </div>
-            </div>
+            <?php endwhile; ?>
+            <?php endif; ?>
+            <!-- ループ終了 -->
         </div>
     </div>
 </section>
