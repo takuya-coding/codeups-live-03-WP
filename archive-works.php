a8346a69ed01b0363cd55ac249538b6e1c1ebb38
takuya <awa08082023@gmail.com>
2023-12-20 05:49:06 +0900

    archive-works.php作成開始

---
+++ archive-works.php
@@ -0,0 +1,65 @@
+<?php get_header(); ?>
+
+<!-- 下層ページのメインビュー -->
+<div class="p-sub-mv js-mv-height">
+    <div class="p-sub-mv__title">
+        <div class="c-section-header--white">
+            <h1 class="c-section-header__engtitle">works</h1>
+            <p class="c-section-header__jatitle">制作実績</p>
+        </div>
+    </div>
+</div>
+
+<!-- パンくず -->
+<?php get_template_part( 'breadcrumb' ); ?>
+
+
+<section class="l-archive-works p-archive-works">
+    <div class="p-archive-works__inner l-inner">
+        <div class="p-archive-works__items">
+            <!-- ループ -->
+            <div class="p-archive-works__item">
+                <div class="p-archive-works__img">
+                    <img src="./images/common/works_1.jpg" alt="">
+                </div>
+                <div class="p-archive-works__title">
+                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
+                </div>
+            </div>
+            <div class="p-archive-works__item">
+                <div class="p-archive-works__img">
+                    <img src="./images/common/works_1.jpg" alt="">
+                </div>
+                <div class="p-archive-works__title">
+                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
+                </div>
+            </div>
+            <div class="p-archive-works__item">
+                <div class="p-archive-works__img">
+                    <img src="./images/common/works_1.jpg" alt="">
+                </div>
+                <div class="p-archive-works__title">
+                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
+                </div>
+            </div>
+            <div class="p-archive-works__item">
+                <div class="p-archive-works__img">
+                    <img src="./images/common/works_1.jpg" alt="">
+                </div>
+                <div class="p-archive-works__title">
+                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
+                </div>
+            </div>
+            <div class="p-archive-works__item">
+                <div class="p-archive-works__img">
+                    <img src="./images/common/works_1.jpg" alt="">
+                </div>
+                <div class="p-archive-works__title">
+                    <h2 class="p-archive-works__name">Smoothiesta 様</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
